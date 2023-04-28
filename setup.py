fcfb5b74c96f78bc35ab783b566842c6f2d323ca
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 14:58:02 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Setup script for pkg package.
+"""
+
+from setuptools import setup
+
+setup(name='openalex',
+      version='0.0.1',
+      description='bibtex utilities',
+      maintainer='saaksshi',
+      maintainer_email='sjilhewa@andrew.cmu.edu',
+      license='MIT',
+      packages=['openalex'],
+      scripts=[],
+      long_description='''assignment''')
