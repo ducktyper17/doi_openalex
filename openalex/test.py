5d65b86ab38adbe95ee6ceab1535c405a485bce1
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 18:52:38 -0400

    Update test.py

--- openalex/test.py
+++ openalex/test.py
@@ -5,40 +5,32 @@ of CitationShark are working properly.
 import re
 from .works import Works
 
-
-def test_bibtex():
-    """
-    Test to see if bibtex returns a bibtex file. ChatGPT 4 assisted
-    me in writing this function.
-    """
-    w_0 = Works("https://doi.org/10.1021/jp047349j")
-    bibtex_entry = w_0.bibtex()
-    # Define a simple pattern to check if the
-    # returned string is a BibTeX entry
-    bibtex_pattern = re.compile(r"@\w+\{[^}]+\,[\s\S]*\}", re.MULTILINE)
-
-    # Check if the returned string matches the pattern
-    assert (
-        bibtex_pattern.match(bibtex_entry) is not None
-    ), "The returned string is not a valid BibTeX entry"
+ref_ris = """TY  - JOUR
+AU  - John R. Kitchin
+PY  - 2015
+TI  - Examples of Effective Data Sharing in Scientific Publishing
+JO  - ACS Catalysis
+VL  - 5
+IS  - 6
+SP  - 3894
+EP  - 3899
+DO  - https://doi.org/10.1021/acscatal.5b00538
+ER  -"""
 
 
 def test_ris():
-    """
-    Test to see if bibtex returns a bibtex file. ChatGPT 4 assisted
-    me in writing this function.
-    """
-    # Call the function you want to test
-    w_0 = Works("https://doi.org/10.1021/jp047349j")
-    ris_citation = w_0.ris()
+    w = Works("https://doi.org/10.1021/acscatal.5b00538")
+    assert ref_ris == w.ris
 
-    # Define a simple pattern to check if the
-    # returned string is an RIS citation
-    ris_pattern = re.compile(
-        r"^TY\s+-\s+\w+(\n[A-Z0-9]{2}\s+-\s+[\s\S]+?)*\nER\s+-", re.MULTILINE
-    )
+   
+ref_bibtex = """author = {John R. Kitchin},
+ journal = {ACS Catalysis},
+ title = {Examples of Effective Data Sharing in Scientific Publishing},
+ volume  ={5},
+ issue  = {6},  
+ pages = {3894-3899},
+ year = {2015}"""
 
-    # Check if the returned string matches the pattern
-    assert (
-        ris_pattern.match(ris_citation) is not None
-    ), "The returned string is not a valid RIS citation"
+def test_bibtex():
+    w = Works("https://doi.org/10.1021/acscatal.5b00538")
+    assert ref_bibtex == w.bibtex()
