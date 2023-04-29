a31ab79366f333033b8a06a95717f2f41b2a0b99
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 21:46:44 -0400

    Update works.py

--- openalex/works.py
+++ openalex/works.py
@@ -23,72 +23,47 @@ class Works:
         """
         self.oaid = oaid
         self.req = requests.get(f'https://api.openalex.org/works/{oaid}')
-        self.data = self.req.json
-       
-    def bibtex(self):
-        author = self.data['authorship']['author']['display_name']
+        self.data = self.req.json()
+    def new(self):
         title = self.data['title']
-        journal = self.data['host_venue']['display_name']
+        return title
+
+    def bibtex(self):
+        """
+        Returns the BibTeX string for the work.
+
+        Returns:
+            str: The BibTeX string.
+        """
+        _authors = [au['author']['display_name'] for au in self.data['authorships']]
+        if len(_authors) == 1:
+            authors = _authors[0]
+        else:
+            authors = ', '.join(_authors[0:-1]) + ' and' + _authors[-1]
+
+        title = self.data['title']
+
         volume = self.data['biblio']['volume']
+
         issue = self.data['biblio']['issue']
+        if issue is None:
+            issue = ', '
+        else:
+            issue = ', ' + issue
+
         pages = '-'.join([self.data['biblio']['first_page'], self.data['biblio']['last_page']])
         year = self.data['publication_year']
-        citedby = self.data['cited_by_count']
-        date_added = self.data['updated_date']
-        doi = self.data['doi']
-        number = issue
-        oa = self.data['id']
-        url = self.oaid
-        date_added = self.data['updated_date']
-        doi = self.data['doi']
-        number = issue
 
-        bibtex = f"""author = {author}, 
-        journal = {journal}, 
-        title = {title},
-        volume = {volume},
-        issue = {issue}, 
-        pages = {pages},
-        year = {year}"""
- 
-        return bibtex
+        seq = (
+            f'\nauthor = {authors},\n'
+            f'title = {title},\n'
+            f'volume = {volume},\n'
+            f'number = {issue},\n'
+            f'pages = {pages},\n'
+            f'year = {year},\n'
+            f'doi = "{self.data["doi"]}",\n'
+            f'url = "{self.oaid}",\n'
+            f'DATE_ADDED = {self.data["updated_date"]}'
+        )
 
-              
-
-            
-#     def ris(self):
-#         """
-#         Returns the ris  for the work.
-
-#         Returns:
-#             html: The ris string.
-#         """
-#         fields = []
-#         if self.data['type'] == 'journal-article':
-#             fields += ['TY  - JOUR']
-#         else:
-#             raise Exception("Unsupported type {self.data['type']}")
-        
-#         for author in self.data['authorships']:
-#             fields += [f'AU  - {author["author"]["display_name"]}']
-            
-#         fields += [f'PY  - {self.data["publication_year"]}']
-#         fields += [f'TI  - {self.data["title"]}']
-#         fields += [f'JO  - {self.data["host_venue"]["display_name"]}']
-#         fields += [f'VL  - {self.data["biblio"]["volume"]}']
-        
-#         if self.data['biblio']['issue']:
-#             fields += [f'IS  - {self.data["biblio"]["issue"]}']
-        
-        
-#         fields += [f'SP  - {self.data["biblio"]["first_page"]}']
-#         fields += [f'EP  - {self.data["biblio"]["last_page"]}']
-#         fields += [f'DO  - {self.data["doi"]}']
-#         fields += ['ER  -']
-                
-#         ris = '\n'.join(fields)
-#         ris64 = base64.b64encode(ris.encode('utf-8')).decode('utf8')
-#         uri = (f'<pre>{ris}<pre><br>'
-#                f'<a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>')
-#         return uri
-            
+        return seq
