127783a073329ae1eeb3619845fa238b0db7e156
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 22:57:38 -0400

    Update test.py

--- openalex/test.py
+++ openalex/test.py
@@ -1,15 +1,12 @@
 """
 Tests to ensure pacakge is working.
 """
-import re
 from .works import Works
-
-
-title = "Examples of Effective Data Sharing in Scientific Publishing"
+TITLE = "Examples of Effective Data Sharing in Scientific Publishing"
 
 def test_bibtex():
     """
     Tests to check working function in package.
     """
     won = Works("https://doi.org/10.1021/acscatal.5b00538")
-    assert title == won.new()
+    assert TITLE == won.new()
