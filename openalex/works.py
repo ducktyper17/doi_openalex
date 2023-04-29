fbff3acc2e35abe143132ba386cef9011e05d0d9
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 20:27:31 -0400

    Update works.py

--- openalex/works.py
+++ openalex/works.py
@@ -63,39 +63,39 @@ class Works:
               
 
             
-    def ris(self):
-        """
-        Returns the ris  for the work.
+#     def ris(self):
+#         """
+#         Returns the ris  for the work.
 
-        Returns:
-            html: The ris string.
-        """
-        fields = []
-        if self.data['type'] == 'journal-article':
-            fields += ['TY  - JOUR']
-        else:
-            raise Exception("Unsupported type {self.data['type']}")
+#         Returns:
+#             html: The ris string.
+#         """
+#         fields = []
+#         if self.data['type'] == 'journal-article':
+#             fields += ['TY  - JOUR']
+#         else:
+#             raise Exception("Unsupported type {self.data['type']}")
         
-        for author in self.data['authorships']:
-            fields += [f'AU  - {author["author"]["display_name"]}']
+#         for author in self.data['authorships']:
+#             fields += [f'AU  - {author["author"]["display_name"]}']
             
-        fields += [f'PY  - {self.data["publication_year"]}']
-        fields += [f'TI  - {self.data["title"]}']
-        fields += [f'JO  - {self.data["host_venue"]["display_name"]}']
-        fields += [f'VL  - {self.data["biblio"]["volume"]}']
+#         fields += [f'PY  - {self.data["publication_year"]}']
+#         fields += [f'TI  - {self.data["title"]}']
+#         fields += [f'JO  - {self.data["host_venue"]["display_name"]}']
+#         fields += [f'VL  - {self.data["biblio"]["volume"]}']
         
-        if self.data['biblio']['issue']:
-            fields += [f'IS  - {self.data["biblio"]["issue"]}']
+#         if self.data['biblio']['issue']:
+#             fields += [f'IS  - {self.data["biblio"]["issue"]}']
         
         
-        fields += [f'SP  - {self.data["biblio"]["first_page"]}']
-        fields += [f'EP  - {self.data["biblio"]["last_page"]}']
-        fields += [f'DO  - {self.data["doi"]}']
-        fields += ['ER  -']
+#         fields += [f'SP  - {self.data["biblio"]["first_page"]}']
+#         fields += [f'EP  - {self.data["biblio"]["last_page"]}']
+#         fields += [f'DO  - {self.data["doi"]}']
+#         fields += ['ER  -']
                 
-        ris = '\n'.join(fields)
-        ris64 = base64.b64encode(ris.encode('utf-8')).decode('utf8')
-        uri = (f'<pre>{ris}<pre><br>'
-               f'<a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>')
-        return uri
+#         ris = '\n'.join(fields)
+#         ris64 = base64.b64encode(ris.encode('utf-8')).decode('utf8')
+#         uri = (f'<pre>{ris}<pre><br>'
+#                f'<a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>')
+#         return uri
             
