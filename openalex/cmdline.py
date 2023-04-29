42f053ec609460896dd03400b855fe1e7525dbbd
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 23:33:59 -0400

    Update cmdline.py

--- openalex/cmdline.py
+++ openalex/cmdline.py
@@ -1,12 +1,11 @@
 """
-This module creates a RIS and BibTex command line interface.
+This module command line interface.
 """
 
 #!/usr/bin/env python3
 
 import click
 from .works import Works
-
 
 @click.command()
 @click.argument("doi")
@@ -21,8 +20,8 @@ def cite(doi, citeformat):
     Outputs RIS or BibTeX for a given DOI. ChatGPT 4 assisted
     me in writing this function.
     """
-    w_0 = Works(doi)
+    wrk = Works(doi)
     if citeformat == "bibtex":
-        print(w_0.bibtex())
+        print(wrk.bibtex())
     elif citeformat == "ris":
-        print(w_0.ris())
+        print(wrk.ris())
