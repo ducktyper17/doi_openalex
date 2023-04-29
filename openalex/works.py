d9a79b335bdbc800ebc21764eb9ec5e36eb0afce
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 22:55:34 -0400

    Update works.py

--- openalex/works.py
+++ openalex/works.py
@@ -5,6 +5,7 @@ Module for working with scholarly works from the OpenAlex API.
 import base64
 import requests
 
+
 class Works:
     """
     A class representing a scholarly work.
@@ -22,13 +23,11 @@ class Works:
             oaid (str): The OpenAlex identifier of the work.
         """
         self.oaid = oaid
-        self.req = requests.get(f'https://api.openalex.org/works/{oaid}')
+        self.req = requests.get(f"https://api.openalex.org/works/{oaid}")
         self.data = self.req.json()
-        
+
     def new(self):
-        titles = self.data['title']
-        volume = self.data['biblio']['volume']
-        issue = self.data['biblio']['issue']
+        titles = self.data["title"]
         return titles
 
     def bibtex(self):
@@ -38,38 +37,41 @@ class Works:
         Returns:
             str: The BibTeX string.
         """
-        _authors = [au['author']['display_name'] for au in self.data['authorships']]
+        _authors = [au["author"]["display_name"] for au in self.data["authorships"]]
         if len(_authors) == 1:
             authors = _authors[0]
         else:
-            authors = ', '.join(_authors[0:-1]) + ' and' + _authors[-1]
+            authors = ", ".join(_authors[0:-1]) + " and" + _authors[-1]
 
-        title = self.data['title']
+        title = self.data["title"]
 
-        volume = self.data['biblio']['volume']
+        volume = self.data["biblio"]["volume"]
 
-        issue = self.data['biblio']['issue']
+        issue = self.data["biblio"]["issue"]
         if issue is None:
-            issue = ', '
+            issue = ", "
         else:
-            issue = ', ' + issue
+            issue = ", " + issue
 
-        pages = '-'.join([self.data['biblio']['first_page'], self.data['biblio']['last_page']])
-        year = self.data['publication_year']
+        pages = "-".join(
+            [self.data["biblio"]["first_page"], self.data["biblio"]["last_page"]]
+        )
+        year = self.data["publication_year"]
 
         seq = (
-            f'author = {authors},\n'
-            f'title = {title},\n'
-            f'volume = {volume},\n'
-            f'number = {issue},\n'
-            f'pages = {pages},\n'
-            f'year = {year},\n'
+            f"author = {authors},\n"
+            f"title = {title},\n"
+            f"volume = {volume},\n"
+            f"number = {issue},\n"
+            f"pages = {pages},\n"
+            f"year = {year},\n"
             f'doi = "{self.data["doi"]}",\n'
             f'url = "{self.oaid}",\n'
             f'DATE_ADDED = {self.data["updated_date"]}'
         )
 
         return seq
+
     def ris(self):
         """
         Returns the ris  for the work.
@@ -78,12 +80,12 @@ class Works:
             html: The ris string.
         """
         fields = []
-        if self.data['type'] == 'journal-article':
-            fields += ['TY  - JOUR']
+        if self.data["type"] == "journal-article":
+            fields += ["TY  - JOUR"]
         else:
             raise Exception("Unsupported type {self.data['type']}")
 
-        for author in self.data['authorships']:
+        for author in self.data["authorships"]:
             fields += [f'AU  - {author["author"]["display_name"]}']
 
         fields += [f'PY  - {self.data["publication_year"]}']
@@ -91,17 +93,18 @@ class Works:
         fields += [f'JO  - {self.data["host_venue"]["display_name"]}']
         fields += [f'VL  - {self.data["biblio"]["volume"]}']
 
-        if self.data['biblio']['issue']:
+        if self.data["biblio"]["issue"]:
             fields += [f'IS  - {self.data["biblio"]["issue"]}']
-
 
         fields += [f'SP  - {self.data["biblio"]["first_page"]}']
         fields += [f'EP  - {self.data["biblio"]["last_page"]}']
         fields += [f'DO  - {self.data["doi"]}']
-        fields += ['ER  -']
+        fields += ["ER  -"]
 
-        ris = '\n'.join(fields)
-        ris64 = base64.b64encode(ris.encode('utf-8')).decode('utf8')
-        uri = (f'<pre>{ris}<pre><br>'
-               f'<a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>')
+        ris = "\n".join(fields)
+        ris64 = base64.b64encode(ris.encode("utf-8")).decode("utf8")
+        uri = (
+            f"<pre>{ris}<pre><br>"
+            f'<a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>'
+        )
         return uri
