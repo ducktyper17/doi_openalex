e55bb81ddff7e772a8df738e039e5fc8f8592999
ducktyper17 <112985642+ducktyper17@users.noreply.github.com>
2023-04-28 20:55:26 -0400

    Update test.py

--- openalex/test.py
+++ openalex/test.py
@@ -21,12 +21,12 @@ ER  -"""
 #     assert ref_ris == w.ris
     
 ref_bibtex = """author = John R. Kitchin,
- journal = ACS Catalysis,
- title = Examples of Effective Data Sharing in Scientific Publishing,
- volume  = 5,
- issue  = 6,  
- pages = 3894-3899,
- year = 2015"""
+journal = ACS Catalysis,
+title = Examples of Effective Data Sharing in Scientific Publishing,
+volume  = 5,
+issue  = 6,
+pages = 3894-3899,
+year = 2015"""
 
 def test_bibtex():
     w = Works("https://doi.org/10.1021/acscatal.5b00538")
